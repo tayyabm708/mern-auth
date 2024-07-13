f898b13ccd31be98d6ab47dc3498d6ce426651f4
Muhammad Tayyab <tayyabm708@gmail.com>
2024-07-13 22:02:05 +0500

    add a middleware and functions to handle errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -3,7 +3,7 @@ import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
 
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPasword = bcryptjs.hashSync(password, 10);
   const newUser = new User({ username, email, password: hashedPasword });
@@ -13,6 +13,6 @@ export const signup = async (req, res) => {
       res.status(201).json({message: "User created successfully"});
     
   } catch (error) {
-    res.status(500).json(error.message);
+    next(error);
   }
 };
--- api/index.js
+++ api/index.js
@@ -4,7 +4,6 @@ import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 
-
 dotenv.config();
 
 mongoose
@@ -26,3 +25,14 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error"
+    return res.status(statusCode).json({
+      success: false,
+      message,
+      statusCode,
+
+  })
+});
