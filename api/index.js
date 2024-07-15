e2d286a89329ef4a8be425c1ca5cc5c11aeecea0
Muhammad Tayyab <tayyabm708@gmail.com>
2024-07-16 01:41:59 +0500

    add pre production changes

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 dotenv.config();
 
@@ -16,7 +17,14 @@ mongoose
     console.log(err);
   });
 
+const __dirname = path.resolve();
 const app = express();
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use(express.json());
 
@@ -31,11 +39,10 @@ app.use("/api/auth", authRoutes);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = err.message || "Internal Server Error"
-    return res.status(statusCode).json({
-      success: false,
-      message,
-      statusCode,
-
-  })
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    statusCode,
+  });
 });
